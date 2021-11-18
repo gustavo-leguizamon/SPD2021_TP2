544aa4ecb48e004ab2a06826dc1277849c77fbed
Gustavo <Hukummi@live.com>
2021-11-18 17:32:02 -0300

    magage lives

--- main.cpp
+++ main.cpp
@@ -21,16 +21,33 @@
 #define SECONDS_TO_REBOOT 3
 
 void antiBounces(int pinButton, int* stateBefore, void (*callback)());
+int checkFinish();
+void startGame();
+void drawLives(int total);
+void clearLives();
+void drawHiddenWord(char word[]);
+void testLetter();
+void drawMatchingLetters(char word[]);
+int setPosRandomWord();
+void printCurrentLetter();
+void nextLetter();
+void previousLetter ();
+void antiBounces(int pinButton, int* stateBefore, void (*callback)());
+int newButtonPress(int pinButton, int* stateBefore);
+int secondsPassed(int seconds);
+void rebootGame();
 
 LiquidCrystal lcd(LCD_RS, LCD_ENABLE, LCD_DB4, LCD_DB5, LCD_DB6, LCD_DB7);
 
 //Variables para mantener el estado previo de los botones
 int stateButtonUpBefore = LOW;
 int stateButtonTestBefore = LOW;
+int stateButtonDownBefore = LOW;
 
 char currentLetter;
 int posSelectedWord;
 int flagRebootGame;
+int lives = TOTAL_LIVES;
 
 unsigned long previousMillis = 0;
 
@@ -44,6 +61,9 @@ char hiddenWords[AMOUNT_WORDS][20] = {
   "CUCA"
 };
 
+char auxWord [20];
+
+//Se configura el lcd y los pines
 void setup()
 {
   Serial.begin(9600);
@@ -67,69 +87,95 @@ void loop()
     }
   }
   else{
-    /*
     //Si presiona el boton SUBIR LETRA, revisa si se encuentra previamente presionado
-    int stateButtonUpNow = digitalRead(BUTTON_UP);
-    if (stateButtonUpNow == HIGH && stateButtonUpBefore == LOW)
-    {
-      nextLetter();
-      printCurrentLetter();
-    }
-    stateButtonUpBefore = stateButtonUpNow;  //Actualiza el estado del boton
-    */
-
-    //Si presiona el boton SUBIR LETRA, revisa si se encuentra previamente presionado
+  	//Escucha si el boton esta apretado o no. Si esta apretado llama a nextLetter
     antiBounces(BUTTON_UP, &stateButtonUpBefore, nextLetter);
 
     //Si presiona el boton PROBAR LETRA, revisa si se encuentra previamente presionado
+    //Escucha si el boton esta apretado o no. Si esta apretado llama a testLetter
     antiBounces(BUTTON_TEST, &stateButtonTestBefore, testLetter);
-    /*
-    if (newButtonPress(BUTTON_UP, &stateButtonUpBefore)){
-      nextLetter();
-    }
-    */
+    
+    //Si presiona el boton BAJAR LETRA, revisa si se encuentra previamente presionad
+    //Escucha si el boton esta apretado o no. Si esta apretado llama a previousLetter
+    antiBounces(BUTTON_DOWN, &stateButtonDownBefore , previousLetter);
+    
     printCurrentLetter();
+    
+    if(checkFinish() ==1){
+    	//ganaste
+    	lcd.setCursor(4, 1);
+    	lcd.print ("GANASTE");
+    	rebootGame();
+    }
+    else{
+    	if(checkFinish()== -1){
+    		//perdiste
+    		lcd.setCursor(4, 1);
+    		lcd.print ("PERDISTE");
+    		rebootGame();
+    		
+    	}
+    }
+	
+    
   }
+}
+
+//Esta funcion devuelve 1 cuando las palabra ingresada coincide con la palabra seleccionada.
+//Devuelve -1 cuando vidas es igual a 0.
+//Devuelve 0 cuando no sucede ninguna de las condiciones anteriores.
+int checkFinish(){
+	int value = 0; //lo iniciamos en 0 para que no retorne basura.
+	if(lives == 0){
+		value = -1;
+	}
+	else{
+		if(strcmp(hiddenWords[posSelectedWord] , auxWord) == 0){
+			value = 1;
+		}	 
+	}
+	return value;
 }
 
 //Inicia el juego configurando valores por defecto
 void startGame(){
   flagRebootGame = 0;   //Desactiva el flag de reinicio
   currentLetter = 'A';  //Inicializa en la letra A
+  lives = TOTAL_LIVES;
 
   lcd.clear(); //Limpia completo el display para dejarlo en blanco
 
   printCurrentLetter();
-  setPosRandomWord();
+  setPosRandomWord(); //genera un numero random y lo guarda en una variable global llamada posSeledtWord
 
-  drawLives(TOTAL_LIVES);
+  drawLives(lives);
 
-  drawHiddenWord(hiddenWords[posSelectedWord]);
+  drawHiddenWord(hiddenWords[posSelectedWord]); //dibuja las lineas para completar palabra
 }
 
 //Dibuja todas las vidas del jugador, maximo 6
 void drawLives(int total){
-  if (total > 6){
-    total = 6;
-  }
-  else if (total < 1){
-    total = 1;
-  }
-  int posInit = 16 - total - 1;
+  int posInit = 16 - total;
   lcd.setCursor(posInit, 0);
-  lcd.print(":");
-
-  posInit++;
-
+  clearLives(); // limpiamos todas las vidas para volver a dibujarlas sin tener que borrar 1 por 1 
   for (int i = 0; i < total; i++){
     lcd.setCursor(posInit + i, 0);
-    lcd.print("*");
+    lcd.print("*"); 
   }
 }
 
-//Dibuja los caracteres correspondientes a la palabra oculta
+// Limpia las vidas que se muestran en el lcd.
+void clearLives(){
+	for(int i = 16 - TOTAL_LIVES; i <= 16; i++){
+		lcd.setCursor(i, 0);
+		lcd.print(" ");
+	}
+}
+
+//Dibuja las lineas correspondientes a la palabra oculta
+//Recibe la palabra que debe adividar el usuario.
 void drawHiddenWord(char word[]){
-  int wordLength = strlen(word);
+  int wordLength = strlen(word); //Se queda con la longitud de la palabra para saber cuantos espacios dibujar
   int posLcd = (int)((16 - wordLength) / 2); // //Posicion inicial de la palabra en el lcd
   for (int i = 0; i < wordLength; i++){
     lcd.setCursor(posLcd + i, 1);
@@ -142,20 +188,28 @@ void testLetter(){
   drawMatchingLetters(hiddenWords[posSelectedWord]);
 }
 
-//Dibuja las letras de la palabra que coincidan con la letra seleccionada para probar
+//Primero corrobora si la letra (currentLetter) va en esa posicion y en el caso de que si la dibuja 
 void drawMatchingLetters(char word[]){
+  int flagMatch = 0;
   int wordLength = strlen(word);
   int posLcd = (int)((16 - wordLength) / 2); //Posicion inicial de la palabra en el lcd
   for (int i = 0; i < wordLength; i++){
     if (word[i] == currentLetter){
-      lcd.setCursor(posLcd, 1);
-      lcd.print(currentLetter);      
+      flagMatch = 1;
+      lcd.setCursor(posLcd, 1); // 1 es el renglon del led
+      lcd.print(currentLetter);   
+      auxWord[i]= currentLetter; //guardo la palabra que se esta formando.
     }
     posLcd++;
   }
+  if (flagMatch == 0){
+	  //Si entra al if es porque no macheo con ninguna letra y le debe descontar una vida
+	  lives--;
+  }
+  drawLives(lives);
 }
 
-//Selecciona una palabra aleatoria del array de palabras mediante su posicion
+//Arroja un numero random y lo guarda en la variable global postSelectedWord que nos va a servir para elegir la palabra del listado
 int setPosRandomWord(){
   posSelectedWord = random(0, AMOUNT_WORDS);
 }
@@ -177,6 +231,15 @@ void nextLetter(){
   else{
     currentLetter++;
   }
+}
+
+void previousLetter (){
+	if (currentLetter <= 'A'){
+		currentLetter = 'Z';
+	}
+	else{
+		currentLetter--;
+	}
 }
 
 //Revisa si un boton ya se encuentra presionado
@@ -201,7 +264,6 @@ int newButtonPress(int pinButton, int* stateBefore){
   return wasPressed;
 }
 
-
 //Indica si pasaron la cantidad de segundos indicada en el parametro
 int secondsPassed(int seconds){
   int passed = 0;
@@ -212,7 +274,6 @@ int secondsPassed(int seconds){
   }
   return passed;
 }
-
 
 //Marca el juego para reiniciar
 void rebootGame(){
